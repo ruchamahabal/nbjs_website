35e88a8c8f60daec727a815815313924882aeb7e
cybersagex <cybersage@protonmail.com>
2018-09-07 18:55:14 +0530

    Started working

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <!-- Title -->
+    <title>Nanbhat Janhit Society</title>
+
+    <!-- Bootstrap css -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+  </head>
+  <body>
+    Header here
+    <!-- Navbar -->
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include_once 'header.php' ?>
+
+  Index.php here
+  <!-- Carousel slider -->
+
+  <!-- Office bearer 2017 2018 images -->
+
+  <!-- Membership registration form -->
+
+<?php include_once 'footer.php' ?>
