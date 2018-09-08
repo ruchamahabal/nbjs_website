b254f9e75c60026551e3ad925d965eb56bdc08d8
cybersagex <cybersage@protonmail.com>
2018-09-08 08:12:38 +0530

    Basic structure created...Frontend

--- header.php
+++ header.php
@@ -10,5 +10,55 @@
 
   </head>
   <body>
-    Header here
+    
     <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light" style="padding:auto 5%;">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About Us</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Members List
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">2017 - 2020</a>
+              <a class="dropdown-item" href="#">2014 - 2017</a>
+              <a class="dropdown-item" href="#">2011 - 2014</a>
+              <a class="dropdown-item" href="#">2008 - 2011</a>
+              <a class="dropdown-item" href="#">2005 - 2008</a>
+            </div>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" data-hover="dropdown-menu" aria-haspopup="true" aria-expanded="false">
+              Events Photo Gallary
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">2017 - 2018</a>
+              <a class="dropdown-item" href="#">2065 - 2017</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Event Registration</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Membership Registration Form</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Feedback</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
--- index.php
+++ index.php
@@ -1,10 +1,42 @@
+<!-- Header -->
 <?php include_once 'header.php' ?>
 
-  Index.php here
   <!-- Carousel slider -->
+  <div id="slider" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#slider" data-slide-to="0" class="active"></li>
+      <li data-target="#slider" data-slide-to="1"></li>
+      <li data-target="#slider" data-slide-to="2"></li>
+      <li data-target="#slider" data-slide-to="3"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img class="d-block w-100" src=".../800x400?auto=yes&bg=777&fg=555&text=First slide" alt="First slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src=".../800x400?auto=yes&bg=666&fg=444&text=Second slide" alt="Second slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src=".../800x400?auto=yes&bg=555&fg=333&text=Third slide" alt="Third slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src=".../800x400?auto=yes&bg=555&fg=333&text=Third slide" alt="Fourth slide">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
 
   <!-- Office bearer 2017 2018 images -->
+  <?php include_once 'officeBearer.php' ?>
 
   <!-- Membership registration form -->
 
+<!-- Footer -->
 <?php include_once 'footer.php' ?>
